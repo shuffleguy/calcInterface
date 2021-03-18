53059d72901f7411df25bddb099bc3ba9a187a02
lvinicio <lucas.vinicius.oliveira@everis.com>
2021-03-18 11:13:39 -0300

    Iniciando projeto de calculadora com interface

---
+++ Calc.ws.kts
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main() {
+
+    println("-------------------")
+    println("   CALCULADORA     ")
+    println("-------------------")
+
+
+}
+
+fun sum(a1:Float,a2:Float) = a1.plus(a2)
+fun sub(a1:Float,a2:Float) = a1.minus(a2)
+fun mult(a1:Float,a2:Float) = a1.times(a2)
+fun div(a1:Float,a2:Float) = a1.div(a2)
